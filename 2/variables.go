1761d277178102e3ede7d7ba5e491568949bc58c
Abhinav Verma <abhicsmnnit@gmail.com>
2024-02-13 14:38:29 +0530

    Check overflow for various types

--- 2/variables.go
+++ 2/variables.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello variables")
+	var b byte = 255
+	fmt.Println(b)
+	fmt.Println(b + 1) // Wraps around to 0
+
+	var smallI int32 = 2_147_483_647
+	fmt.Println(smallI)
+	fmt.Println(smallI + 1) // Wraps around to -2_147_483_648
+
+	var bigI uint64 = 18_446_744_073_709_551_615
+	fmt.Println(bigI)
+	fmt.Println(bigI + 1) // Wraps around to 0
 }

9134709b8ab1b31b58a4588cadd5ccf5ca60ffc5
Abhinav Verma <abhicsmnnit@gmail.com>
2024-02-16 11:06:34 +0530

    Playground code for switch in Golang

--- 4/playground.go
+++ 4/playground.go
@@ -106,19 +106,59 @@ func main() {
 	// }
 	// fmt.Println(evenVals)
 
-	samples := []string{"hello", "apple_π!"}
-outer:
-	for _, sample := range samples {
-	inner:
-		for i, r := range sample {
-			if r == 'e' {
-				continue inner
-			}
-			fmt.Println(i, r, string(r))
-			if r == 'l' {
-				continue outer
-			}
+	// 	samples := []string{"hello", "apple_π!"}
+	// outer:
+	// 	for _, sample := range samples {
+	// 	inner:
+	// 		for i, r := range sample {
+	// 			if r == 'e' {
+	// 				continue inner
+	// 			}
+	// 			fmt.Println(i, r, string(r))
+	// 			if r == 'l' {
+	// 				continue outer
+	// 			}
+	// 		}
+	// 		fmt.Println()
+	// 	}
+
+	// words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+	// for _, word := range words {
+	// 	switch size := len(word); size {
+	// 	case 1, 2, 3, 4:
+	// 		fmt.Println(word, "is a short word!")
+	// 	case 5:
+	// 		wordLen := len(word)
+	// 		fmt.Println(word, "is exactly the right length:", wordLen)
+	// 	case 6, 7, 8, 9:
+	// 	default:
+	// 		fmt.Println(word, "is a long word!")
+	// 	}
+	// }
+
+	// words := []string{"hi", "salutations", "hello"}
+	// for _, word := range words {
+	// 	switch wordLen := len(word); { // Blank switch
+	// 	case wordLen < 5:
+	// 		fmt.Println(word, "is a short word!")
+	// 	case wordLen > 10:
+	// 		fmt.Println(word, "is a long word!")
+	// 	default:
+	// 		fmt.Println(word, "is exactly the right length.")
+	// 	}
+	// }
+
+	// FizzBuzz is so clean with switch in golang
+	for i := 1; i <= 20; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
 		}
-		fmt.Println()
 	}
 }

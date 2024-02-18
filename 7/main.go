bd5e96138d6d04d4b3974c2a13edfa2e2275009a
Abhinav Verma <abhicsmnnit@gmail.com>
2024-02-18 21:10:42 +0530

    Chapter 7 playground - more changes

--- 7/main.go
+++ 7/main.go
@@ -33,13 +33,34 @@ func main() {
 	// p := Person{"Abhinav", "Verma", 30}
 	// fmt.Println(p.String())
 
-	c := Counter{}
-	fmt.Println(c.String())
-	c.Increment() // Go automatically converts to (&c).Increment()
-	fmt.Println(c.String())
+	// c := Counter{}
+	// fmt.Println(c.String())
+	// c.Increment() // Go automatically converts to (&c).Increment()
+	// fmt.Println(c.String())
 
-	c1 := &Counter{}
-	fmt.Println(c1.String()) // Go automatically converts to (*c1).String()
-	c1.Increment()
-	fmt.Println(c1.String()) // Go automatically converts to (*c1).String()
+	// c1 := &Counter{}
+	// fmt.Println(c1.String()) // Go automatically converts to (*c1).String()
+	// c1.Increment()
+	// fmt.Println(c1.String()) // Go automatically converts to (*c1).String()
+
+	// var c2 *Counter
+	// fmt.Println(c2.String()) // panic: nil pointer dereference
+	// c2.Increment()           // panic: nil pointer dereference
+	// fmt.Println(c2.String()) // panic: nil pointer dereference
+
+	var c Counter
+	doUpdateWrong(c)
+	fmt.Println("in main:", c.String())
+	doUpdateRight(&c)
+	fmt.Println("in main:", c.String())
+}
+
+func doUpdateWrong(c Counter) {
+	c.Increment()
+	fmt.Println("in doUpdateWrong:", c.String())
+}
+
+func doUpdateRight(c *Counter) {
+	c.Increment()
+	fmt.Println("in doUpdateRight:", c.String())
 }

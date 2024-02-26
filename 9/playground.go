430a7d2f26c17af41fdb1d979250a845183473ba
Abhinav Verma <abhicsmnnit@gmail.com>
2024-02-26 09:07:45 +0530

    Wrapping and unwrapping errors

--- 9/playground.go
+++ 9/playground.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"archive/zip"
-	"bytes"
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
-	data := []byte("Not a zip file!")
-	notAZipFile := bytes.NewReader(data)
-	_, err := zip.NewReader(notAZipFile, int64(len(data)))
-	if err == zip.ErrFormat {
-		fmt.Println("Invalid zip file")
+	// // Using sentinel errors
+	// data := []byte("Not a zip file!")
+	// notAZipFile := bytes.NewReader(data)
+	// _, err := zip.NewReader(notAZipFile, int64(len(data)))
+	// if err == zip.ErrFormat {
+	// 	fmt.Println("Invalid zip file")
+	// }
+
+	err := fileChecker("DefinitelyNotAFile.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		if wrappedError := errors.Unwrap(err); wrappedError != nil { // Unwrap the error
+			fmt.Println(wrappedError)
+		}
 	}
+}
+
+func fileChecker(name string) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("in fileChecker: %w", err) // Wrap the error
+	}
+	f.Close()
+	return nil
 }

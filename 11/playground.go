8a4c93eb41932f81eb6c0140ea4a73d87c2112a8
Abhinav Verma <abhicsmnnit@gmail.com>
2024-03-04 08:36:01 +0530

    Playing with staticcheck tool

--- 11/playground.go
+++ 11/playground.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	s := fmt.Sprintf("Hello") // highlighted by staticcheck tool - go vet lets it pass
+	fmt.Println(s)
+
+	err := returnErr(false)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = returnErr(true) // 2 issues highligted by staticcheck here
+	fmt.Println("end of program")
+}
+
+func returnErr(hitMe bool) error {
+	if hitMe {
+		return errors.New("error has occurred")
+	}
+	return nil
 }

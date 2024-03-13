a1ab11be0d92373bf5a6f9992af42f65fee171eb
Abhinav Verma <abhicsmnnit@gmail.com>
2024-03-13 08:06:29 +0530

    Run code exactly once using sync.OnceValue

---
+++ 12/exactly_once_lazy_load_sync_value/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// Calling Parse method multiple times will initialize the parser only once.
+	// This can be confirmed by verifying that "Initializing..." is printed only once when this program is executed.
+	fmt.Println(Parse("Abhinav"))
+	fmt.Println(Parse("Verma"))
+	fmt.Println(Parse(""))
+}
+
+type Parser interface {
+	Parse(string) string
+}
+
+var initParserCached func() Parser = sync.OnceValue(initParser)
+
+func Parse(dataToParse string) string {
+	parser := initParserCached()
+	return parser.Parse(dataToParse)
+}
+
+func initParser() Parser {
+	// do all sorts of setup and loading here
+	fmt.Println("Initializing...")
+	return SCPI{}
+}
+
+type SCPI struct {
+}
+
+func (s SCPI) Parse(in string) string {
+	if len(in) > 1 {
+		return in[0:1]
+	}
+	return ""
+}
